a4a3a524dfbcffc4fe0870cde99e2cf062cc4482
10Sn0w25 <adriansnow143@gmail.com>
2025-02-19 12:06:28 -0500

    yeah

--- Personal_Project/Assets/Scripts/Enemies/Enemy.cs
+++ Personal_Project/Assets/Scripts/Enemies/Enemy.cs
@@ -3,17 +3,43 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [Header ("Navmesh Data")]
     public NavMeshAgent agent;
     public Transform destination;
+
+    [Header("Enemy Data")]
+    public EnemyData data;
+    private int health;
+    private float speed;
+    private string enemyType;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        speed = data.speed;
+        health = data.health;
+        enemyType = checkEnemyType();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.speed = speed;
         agent.SetDestination(destination.position);
+    }
+    public string checkEnemyType()
+    {
+        if (data.vampire == true)
+        {
+            return "vampire";
+        }
+        else if (data.werewolf == true)
+        {
+            return "were";
+        }
+        else
+        {
+            return 3;
+        }
     }
 }
---
+++ Personal_Project/Assets/Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [Header ("Enemy Stats")]
+    public int health;
+    public float speed;
+
+    [Header("Enemy Type")]
+    public bool vampire;
+    public bool zombie;
+    public bool werewolf;
+}
