3437086807621c308c06a36d277c908a816c2a8b
ItsSn0w <adriansnow143@gmail.com>
2025-05-09 12:06:35 -0400

    Update MenuController.cs

--- Personal_Project/Assets/UI/Menu/MenuController.cs
+++ Personal_Project/Assets/UI/Menu/MenuController.cs
@@ -1,12 +1,28 @@
+using GLTFast.Schema;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    private SceneManager sceneManager;
     private UIDocument mainMenu;
+    private VisualElement root;
+    private Button playButton;
+    private Button exitButton;
     void Awake()
     {
+        
         mainMenu = GetComponentInParent<UIDocument>();
+        root= mainMenu.rootVisualElement;
+
+        playButton = root.Q<Button>("PlayButton");
+        playButton.clicked += playButtonClicked;
+
+        exitButton = root.Q<Button>("ExitButton");
+        exitButton.clicked += exitButtonClicked;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,4 +35,18 @@ public class MenuController : MonoBehaviour
     {
         
     }
+
+    void exitButtonClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+
+    void playButtonClicked()
+    {
+
+    }
+
 }
