facc34635da7dcd31fef0f0ecbe948a27202b68c
ItsSn0w <adriansnow143@gmail.com>
2025-05-27 19:00:55 -0400

    UI UPDATES

--- Personal_Project/Assets/UI/GameUI/RoundUI.cs
+++ Personal_Project/Assets/UI/GameUI/RoundUI.cs
@@ -1,26 +1,33 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RoundUI : MonoBehaviour
 {
-    [Header ("Text Elements")]
+    [Header("Text Elements")]
     public TextMeshProUGUI roundCounter;
     public TextMeshProUGUI healthCounter;
     public TextMeshProUGUI moneyCounter;
     [Header("Buttons")]
-    public Canvas pauseMenu;
+    public GameObject pauseMenu;
     public Button pauseButton;
+    public Button exitButton;
+    public Button returnButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
+        pauseButton.onClick.AddListener(pauseButtonClicked);
+        exitButton.onClick.AddListener(exitButtonClicked);
+        returnButton.onClick.AddListener(returnButtonClicked);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void updateRoundCounter(int round)
@@ -32,9 +39,21 @@ public class RoundUI : MonoBehaviour
     {
         healthCounter.text = "Health " + health;
     }
-    
+
     public void updateMoneyCounter(int money)
     {
         moneyCounter.text = "Gold $" + money;
+    }
+    public void exitButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void pauseButtonClicked()
+    {
+        pauseMenu.SetActive(true);
+    }
+    public void returnButtonClicked()
+    {
+        pauseMenu.SetActive(false);
     }
 }
