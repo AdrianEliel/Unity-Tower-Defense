7c5fc5b2337f56bb28ca26613cca0f83e847b238
ItsSn0w <adriansnow143@gmail.com>
2025-06-04 16:47:55 -0400

    map update but i hate it

--- Personal_Project/Assets/Scripts/Enemies/Enemy.cs
+++ Personal_Project/Assets/Scripts/Enemies/Enemy.cs
@@ -77,7 +77,7 @@ public class Enemy : MonoBehaviour
 
     private IEnumerator moveToWayPoints()
     {
-        while (atTheEnd == false)
+         while (atTheEnd == false)
         {
             yield return null;
             if (roundManager.enemyWaypoints.Length > 1 && roundManager.enemyWaypoints[currentWaypoint] != null)
